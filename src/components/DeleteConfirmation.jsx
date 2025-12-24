b7daec0c3d9c9849cf8464684f192bc413f32bfb
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-24 21:19:06 +0530

    adding a progress bar to the modal

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,19 +1,9 @@
 import { useEffect, useState } from 'react';
+import ProgressBar from './ProgressBar';
 
 const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-  useEffect(() => {
-    const interval = setInterval(
-      () => setRemainingTime((prev) => prev - 10),
-      10
-    );
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
   useEffect(() => {
     console.log('TIMER SET!');
     const timer = setTimeout(() => {
@@ -37,7 +27,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER}></ProgressBar>
     </div>
   );
 }
